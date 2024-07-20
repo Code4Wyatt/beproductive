3ed832de8937ac5ce10eeda0008928217fa47933
Paul Murray <44061707+Code4Wyatt@users.noreply.github.com>
2024-07-20 21:51:09 +0100

    Created inviteUserToDocument and removeUserFromDocument server actions

--- beproductive/actions/actions.ts
+++ beproductive/actions/actions.ts
@@ -51,3 +51,44 @@ export async function deleteDocument(roomId: string) {
         return { success: false }
     }
 }
+
+export async function inviteUserToDocument(roomId: string, email: string) {
+    auth().protect()
+
+    try {
+        await adminDb
+        .collection("users")
+        .doc(email)
+        .collection("rooms")
+        .doc(roomId)
+        .set({
+            userId: email,
+            role: "editor",
+            createdAt: new Date(),
+            roomId
+        })
+
+        return { success: true }
+    } catch (error) {
+        console.log(error)
+        return { success: false }
+    }
+}
+
+export async function removeUserFromDocument(roomId: string, email: string) {
+    auth().protect()
+
+    try {
+        await adminDb
+        .collection("users")
+        .doc(email)
+        .collection("rooms")
+        .doc(roomId)
+        .delete()
+        
+        return { success: true }
+    } catch (error) {
+        console.log(error)
+        return { success: false }
+    }
+}
